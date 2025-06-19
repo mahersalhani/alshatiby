da7d6b0b20b65bfdb9e87ffdef9d0cf726d2c001
mahersalhani <maheryos5@gmail.com>
2025-06-19 03:40:50 +0300

    set up auth

--- client/src/components/custom/auth/index.ts
+++
@@ -1,5 +0,0 @@
-import { AuthButton } from "./auth-button";
-import { AuthLogoutButton } from "./auth-logout-button";
-import { AuthUserNavButton } from "./auth-user-button";
-
-export { AuthButton, AuthLogoutButton, AuthUserNavButton };
