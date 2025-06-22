96c407e49fb37372e1e436c2d5bfdc4ed98a6da7
mahersalhani <maheryos5@gmail.com>
2025-06-23 02:13:11 +0300

    fix: remove public domain URL and proxy setting from server configuration

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,9 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("RAILWAY_PUBLIC_DOMAIN", "http://localhost:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  proxy: true,
 });
