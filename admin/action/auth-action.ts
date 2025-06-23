43a33a0114e8fdffaec6d8b93193f630187d914f
mahersalhani <maheryos5@gmail.com>
2025-06-23 04:45:24 +0300

    .

--- admin/action/auth-action.ts
+++ admin/action/auth-action.ts
@@ -1,18 +1,21 @@
-"use server";
-import api from "@/lib/axios";
-import { cookies } from "next/headers";
+'use server';
+import { cookies } from 'next/headers';
+
+import api from '@/lib/axios';
 
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
-  path: "/",
-  domain: process.env.HOST ?? "localhost",
+  path: '/',
+  domain: process.env.HOST ?? 'localhost',
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  secure: process.env.NODE_ENV === 'production',
 };
 
 export const loginUser = async (data: any) => {
   try {
-    const user = await api.post("/auth/local", {
+    console.log('loginUser:', process.env.NEXT_PUBLIC_BACKEND_URL);
+
+    const user = await api.post('/auth/local', {
       identifier: data.email,
       password: data.password,
     });
@@ -21,12 +24,14 @@ export const loginUser = async (data: any) => {
       const userData = user.data;
 
       const cookieStore = await cookies();
-      cookieStore.set("auth_token", user.data.jwt, config);
+      cookieStore.set('auth_token', user.data.jwt, config);
 
       return userData;
     }
 
-    throw new Error("Invalid credentials");
+    console.log('Login failed:', process.env.NEXT_PUBLIC_BACKEND_URL);
+
+    throw new Error('Invalid credentials');
   } catch (error) {
     throw new Error(error as string);
   }
