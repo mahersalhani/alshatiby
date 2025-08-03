30b3b14dda15c3701289426159351e224a4562af
mahersalhani <maheryos5@gmail.com>
2025-08-03 21:41:54 +0300

    feat: implement student data table with pagination and search functionality

--- admin/app/[locale]/(protected)/students/page.tsx
+++ admin/app/[locale]/(protected)/students/page.tsx
@@ -1,5 +1,21 @@
+import StudentDataTable from './_components/data-table';
+
+import SiteBreadcrumb from '@/components/site-breadcrumb';
+import { Card, CardContent } from '@/components/ui/card';
+
 const page = () => {
-  return <div> Students</div>;
+  return (
+    <div>
+      <SiteBreadcrumb />
+      <div className="space-y-6">
+        <Card>
+          <CardContent className="p-0">
+            <StudentDataTable />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default page;
