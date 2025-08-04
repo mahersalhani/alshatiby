c9c24bad9992a05fbe41a92d657d985de9e43755
mahersalhani <maheryos5@gmail.com>
2025-08-04 12:10:37 +0300

    feat: simplify security middleware configuration in middlewares.ts

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,20 +1,7 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "script-src": ["https://cdnjs.cloudflare.com"],
-          "media-src": ["https://cdnjs.cloudflare.com"],
-          "img-src": ["https://cdnjs.cloudflare.com"],
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
